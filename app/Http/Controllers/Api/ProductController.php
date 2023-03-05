cf60930f759ed239f678d85938e6f0a88566b07b
Alan-Flores <alanflores@hotmail.cl>
2023-03-05 14:44:09 -0300

    Api.php ProductController OK

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,47 +3,62 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
+        $products = Product::all();
+        return $products;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
-        //
+        $product = new Product();
+        $product->code = $request->code;
+        $product->name = $request->name;
+        $product->stock = $request->stock;
+        $product->buy = $request->buy;
+        $product->sale = $request->sale;
+        $product->brand = $request->brand;
+        $product->group_id = $request->group_id;
+        $product->state = $request->state;
+
+        $product->save();
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(string $id)
     {
-        //
+        $product = Product::find($id);
+        return $product;
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(Request $request, string $id)
     {
-        //
+        $product = Product::findOrFail($request->$id);
+        $product = new Product();
+        $product->code = $request->code;
+        $product->name = $request->name;
+        $product->stock = $request->stock;
+        $product->buy = $request->buy;
+        $product->sale = $request->sale;
+        $product->brand = $request->brand;
+        $product->group_id = $request->group_id;
+        $product->state = $request->state;
+
+        $product->save();
+        return $product;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        $product = Product::destroy($id);
+        return $product;
     }
 }
